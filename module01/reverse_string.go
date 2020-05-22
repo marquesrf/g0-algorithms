aa3f093b7088eeaf1a5270eca7309069aed58cc6
Rafael Marques <marquesrf@protonmail.com>
2020-05-21 21:57:12 -0300

    Module 01 - reverse a string (using string builder, more efficient)

--- module01/reverse_string.go
+++ module01/reverse_string.go
@@ -1,9 +1,13 @@
 package module01
 
+import (
+	"strings"
+)
+
 func Reverse(someString string) string {
-	var reverse string
+	var sb strings.Builder
 	for i := len(someString) - 1; i >= 0; i-- {
-		reverse = reverse + string(someString[i])
+		sb.WriteByte(someString[i])
 	}
-	return reverse
+	return sb.String()
 }
